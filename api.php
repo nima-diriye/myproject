8266fbf11641da8c15c7ae5d551d6e30e60cd988
nima-diriye <56007136+nima-diriye@users.noreply.github.com>
2019-10-01 20:13:21 +0300

    Add files via upload

---
+++ api.php
@@ -0,0 +1,41 @@
+<?php
+class Sum
+{
+    // Constructor
+    public function __construct(){
+        
+    }
+    
+    // Destructor
+    public function __destruct(){
+    
+    }
+//function to get sum
+     function sum ($i , $j){
+
+        $sum =   $i + $j  ;
+
+        return $sum ;
+
+    }
+//function to convert to xml
+    function createXml($sum) {
+
+        $response = '<?xml version="1.0" encoding="utf-8"?>';
+        $response .= '<response><sum>'.$sum.'</sum>';
+    
+                $response = $response.'</response>';
+                return $response;
+     }
+}
+ 
+// Create a new object
+$obj = new Sum;
+ $sum = $obj->sum($_GET['x'],$_GET['y']);
+ header("Content-type: text/xml; charset=utf-8");
+ echo $obj->createXml($sum);
+
+// Destroy the object
+unset($obj);
+ 
+?>
